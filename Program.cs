223ed14b07919d32f9d843f6097b1833aeb9c2c9
Milind452 <mohapatra.milind@gmail.com>
2023-03-09 21:58:33 +0530

    Add primary player singleton class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GuardiansOfTheCode;
+
+PrimaryPlayer player = PrimaryPlayer.Instance;
+Console.WriteLine($"{player.Name} - lvl {player.Level}");
