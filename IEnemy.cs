884a66dab8b7ac2d4837b8f1480171149ae2ca37
Milind452 <mohapatra.milind@gmail.com>
2023-03-10 19:31:23 +0530

    Add enemy interface

---
+++ IEnemy.cs
@@ -0,0 +1,13 @@
+namespace GuardiansOfTheCode
+{
+    public interface IEnemy
+    {
+        int Health { get; set; }
+
+        int Level { get; }
+
+        void Attack(PrimaryPlayer player);
+
+        void Defend(PrimaryPlayer player);
+    }
+}
