1bfe761ccd9e8215fbefda2de848929a881144c5
Milind452 <mohapatra.milind@gmail.com>
2023-03-10 19:38:39 +0530

    Add Giant class

---
+++ Giant.cs
@@ -0,0 +1,21 @@
+namespace GuardiansOfTheCode
+{
+    public class Giant : IEnemy
+    {
+        private int _health;
+        private readonly int _level;
+        public int Health { get => _health; set => _health = value; }
+
+        public int Level => _level;
+
+        public void Attack(PrimaryPlayer player)
+        {
+            Console.WriteLine("Giant attacking " + player.Name);
+        }
+
+        public void Defend(PrimaryPlayer player)
+        {
+            Console.WriteLine("Giant defending from " + player.Name);
+        }
+    }
+}
