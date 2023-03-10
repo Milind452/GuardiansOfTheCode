c76b8598263ddc7716809fe412efdbbd0c2d321d
Milind452 <mohapatra.milind@gmail.com>
2023-03-11 03:14:02 +0530

    Create a basic game board

---
+++ GameBoard.cs
@@ -0,0 +1,37 @@
+namespace GuardiansOfTheCode
+{
+    public class GameBoard
+    {
+        private PrimaryPlayer _player;
+
+        public GameBoard()
+        {
+            _player = PrimaryPlayer.Instance;
+        }
+
+        public void PlayArea(int lvl)
+        {
+            if (lvl == 1){
+                PlayFirstLevel();
+            }
+        }
+
+        public void PlayFirstLevel() {
+            const int currentLvl = 1;
+            List<IEnemy> enemies = new();
+            for(int i = 0; i < 10; i++) 
+            {
+                enemies.Add(EnemyFactory.SpawnZombie(currentLvl));
+            }
+            for(int i = 0; i < 3; i++) 
+            {
+                enemies.Add(EnemyFactory.SpawnWerewolf(currentLvl));
+            }
+
+            foreach (var enemy in enemies)
+            {
+                System.Console.WriteLine(enemy.GetType());
+            }
+        }
+    }
+}
